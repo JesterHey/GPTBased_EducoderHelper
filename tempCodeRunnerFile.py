cad3a296456ddf67c0e7f925e1b1a6045e039400
JesterHey <144512889+JesterHey@users.noreply.github.com>
2023-12-10 09:32:25 +0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    # with open(name,'r',encoding='utf-8') as f:
-    #     data = json.load(f)
-    # for i,j in data.items():
-    #     if 'answer' not in j.keys() or j['answer'] == '':
-    #         # 断言查询结果并抛出异常
-    #         assert False,'答案为空，不予上传'
