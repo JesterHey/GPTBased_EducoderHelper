b3bb6d1e3c1d0468d235a3e70110ca771775912e
JesterHey <144512889+JesterHey@users.noreply.github.com>
2023-12-12 14:30:48 +0800

    Delete demo.py

--- demo.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-np.random.seed(0)
-df1 = pd.DataFrame(np.random.randint(0,10,size=(5, 6)), columns=list('ABCDEF'))
-print(df1)
-print(df1.head(2)[['A','B']])
